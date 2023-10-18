7d575c84fb1877dc2ef7550b3361c10af5417c66
prat112 <kundapurprathiksha@gmail.com>
2023-10-18 21:24:33 +0530

    creating login screen frontend (task-4)

---
+++ public/js/login.js
@@ -0,0 +1,34 @@
+const form=document.getElementById('Data-form');
+const pwShowHide = document.querySelector(".eye-icon");
+form.addEventListener('submit',formSubmit);
+pwShowHide.addEventListener('click',pwdclick);
+
+function pwdclick(){
+  let pwFields = form.querySelector(".password");
+
+      if(pwFields.type === "password"){
+        pwFields.type = "text";
+          pwShowHide.classList.replace("bx-hide", "bx-show");
+          return;
+      } 
+      pwFields.type = "password";
+      pwShowHide.classList.replace("bx-show", "bx-hide");
+}
+
+async function formSubmit(e){
+  try{
+      e.preventDefault();  
+      const details={
+          email:e.target.email.value,
+          password:e.target.password.value
+      }
+      
+      const response=await axios.post(`http://localhost:3100/user/login`,details);  
+          console.log(response.data.message);  
+      
+  }
+  catch(err){ 
+      console.log(err.message);
+      document.body.innerHTML +=`<div style="color:red;">${err.message}</div>`;
+  }
+}
